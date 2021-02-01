dd7a0a13ec1cde8c0d101c02bedb475493ba546e
Heron Rodrigues <heronrs1@gmail.com>
2021-01-31 21:43:03 -0300

    Create api connection and sign in context method

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import AuthContext from '../../context/AuthContext';
+import { AuthContext } from '../../context/AuthContext';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -13,32 +13,44 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 import getValidationErrors from '../../utils/getValidationErros';
 
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
-    const auth = useContext(AuthContext);
-    console.log(auth);
+    const { signIn } = useContext(AuthContext);
 
-    const handleSubmit = useCallback(async (data: any) => {
-        formRef.current?.setErrors({});
+    const handleSubmit = useCallback(
+        async (data: SignInFormData) => {
+            formRef.current?.setErrors({});
 
-        try {
-            const schema = Yup.object().shape({
-                name: Yup.string().required('Nome obrigatório.'),
-                email: Yup.string()
-                    .required('E-mail obrigatório.')
-                    .email('Digite um e-mail válido.'),
-                password: Yup.string().required('Insira sua senha.'),
-            });
+            try {
+                const schema = Yup.object().shape({
+                    name: Yup.string().required('Nome obrigatório.'),
+                    email: Yup.string()
+                        .required('E-mail obrigatório.')
+                        .email('Digite um e-mail válido.'),
+                    password: Yup.string().required('Insira sua senha.'),
+                });
 
-            await schema.validate(data, {
-                abortEarly: false,
-            });
-        } catch (err) {
-            const errors = getValidationErrors(err);
-            formRef.current?.setErrors(errors);
-        }
-    }, []);
+                await schema.validate(data, {
+                    abortEarly: false,
+                });
+
+                signIn({
+                    email: data.email,
+                    password: data.password,
+                });
+            } catch (err) {
+                const errors = getValidationErrors(err);
+                formRef.current?.setErrors(errors);
+            }
+        },
+        [signIn]
+    );
 
     return (
         <Container>
